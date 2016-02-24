4fb242b00fb37b170bf4361c832a2f74cea6efa7
motleytech <motleytechnet@gmail.com>
2016-02-23 23:16:21 -0800

    adding a base template and using if and for statememts in templates

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ views module
 """
 
 from flask import render_template
-from .app import app
+from .app import app # pylint: disable=E0401
 
 @app.route('/')
 @app.route('/index')
@@ -12,5 +12,16 @@ def index():
     The default index view
     """
     user = {'nickname': 'Rajan'}
+    posts = [
+        {
+            'author': {'nickname': 'Nick'},
+            'body': 'This is Nick\'s first awesome post',
+        },
+        {
+            'author': {'nickname': 'Jane'},
+            'body': 'This is Jane\'s first post',
+        }
+    ]
     return render_template('index.html',
-                           user=user)
+                           user=user,
+                           posts=posts)
