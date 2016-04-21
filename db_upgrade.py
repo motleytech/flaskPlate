bdeb631d7bce6925b4f4daa53cc69cbfd0509a83
motleytech <motleytechnet@gmail.com>
2016-04-21 16:54:42 -0700

    fix typo in db upgrade script

--- db_upgrade.py
+++ db_upgrade.py
@@ -3,6 +3,6 @@ from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
 
 api.upgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
-v = api.db_version(SQLALCHEMY_DATA_URI, SQLALCHEMY_MIGRATE_REPO)
+v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
 
 print ('Current database version: %s' % (str(v)))
