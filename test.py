3b51643eed1aa225ee91d40a1c0d27c720fded3a
motleytech <motleytechnet@gmail.com>
2016-04-21 16:34:38 -0700

    removing the test file

--- test.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-
-parser = argparse.ArgumentParser(description='Process input.')
-parser.add_argument('--host', dest='host', default='0.0.0.0',
-                   help='host address')
-
-args = parser.parse_args()
-print args.host
