d67e9fdaefd9c163ccb93d33c29f57c05e7ba45e
motleytech <motleytechnet@gmail.com>
2016-04-21 17:22:44 -0700

    adding support for editing user profile

--- app/forms.py
+++ app/forms.py
@@ -3,10 +3,15 @@ The login form class
 """
 
 from flask.ext.wtf import Form # pylint: disable=E0611,E0401
-from wtforms import StringField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms import StringField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length
 
 class LoginForm(Form):
     "The login form class data fields"
     openid = StringField('openid', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
+
+
+class EditForm(Form):
+    nickname = StringField('nickname', validators=[DataRequired()])
+    about_me = TextAreaField('about_me', validators=[Length(min=0, max=999)])
